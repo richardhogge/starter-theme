3c97dfca809c890e4e5dabf6287dd358abdc0175
Richard Hogge <richard@tremolocreative.com>
2019-10-07 10:51:18 -0700

    add screenshot and theme support for title tags, menus and post thumbnails

--- themes/starter/functions.php
+++ themes/starter/functions.php
@@ -1,5 +1,12 @@
 <?php
 
+// Theme Support
+if ( function_exists( 'add_theme_support' ) ) {
+  add_theme_support( 'title-tag' );
+  add_theme_support( 'menus' );
+  add_theme_support( 'post-thumbnails' );
+}
+
 // Theme CSS
 function starter_enqueue_style() {
 	wp_enqueue_style( 'starter-style', get_template_directory_uri() . '/assets/css/main.css', false );
