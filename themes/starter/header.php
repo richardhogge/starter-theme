d213f2b241cc9fa2fd7fd15acad143e526f1710e
Richard Hogge <richard@tremolocreative.com>
2019-10-06 23:50:02 -0700

    add basic header, footer, index templates

---
+++ themes/starter/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <link rel="profile" href="https://gmpg.org/xfn/11" />
+	<?php wp_head(); ?>
+</head>
+<body <?php // body_class(); ?>>
