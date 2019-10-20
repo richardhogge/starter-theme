6377518a97066880509d5e4b2d3eed4a02bb6fc8
Richard Hogge <richard@tremolocreative.com>
2019-10-20 11:57:34 -0700

    register header and footer menus

--- themes/starter/functions.php
+++ themes/starter/functions.php
@@ -1,22 +1,33 @@
 <?php
 
-// Theme Support
+// Support
 if ( function_exists( 'add_theme_support' ) ) {
   add_theme_support( 'title-tag' );
   add_theme_support( 'menus' );
   add_theme_support( 'post-thumbnails' );
 }
 
-// Theme CSS
+// CSS
 function starter_enqueue_style() {
 	wp_enqueue_style( 'starter-style', get_template_directory_uri() . '/assets/css/main.css', false );
 }
 add_action( 'wp_enqueue_scripts', 'starter_enqueue_style' );
 
-// Theme JS
+// JS
 function starter_enqueue_script() {
 	wp_enqueue_script( 'starter-script', get_stylesheet_directory_uri() . '/assets/js/min/scripts.js', array('jquery') );
 }
 add_action( 'wp_enqueue_scripts', 'starter_enqueue_script' );
+
+// Menus
+function starter_register_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'footer-menu' => __( 'Footer Menu' )
+    )
+  );
+}
+add_action( 'init', 'starter_register_menus' );
 
 ?>
