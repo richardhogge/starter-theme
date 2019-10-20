1c4219d140037826cdca1fdf680933a94afdd9fa
Richard Hogge <richard@tremolocreative.com>
2019-10-20 12:13:53 -0700

    edit markup for index

--- themes/starter/index.php
+++ themes/starter/index.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 <main>
-  <h1><?php the_title(); ?></h1>
+  <h1>
+    <?php the_title(); ?>
+  </h1>
   <?php while ( have_posts() ) : the_post(); ?>
     <?php the_content(); ?>
   <?php endwhile; ?>
